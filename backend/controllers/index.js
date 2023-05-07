098d1109cd8f790d1c990aac477dd1b534c228f5
a-chavez474 <abelinkpact@gmail.com>
2023-05-07 00:48:38 -0600

    updates

--- backend/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const firstFunction = (req, res, next) => {
-    res.json('Melany Chavez');
-};
-
-const secondFunction = (req, res, next) => {
-    res.json('Adela Chavez');
-};
-
-module.exports = { firstFunction, secondFunction};
